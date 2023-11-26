102c80818546109f189286713494eadac49f20b6
Sungchan CHo <kjyy07@naver.com>
2023-11-26 22:07:30 +0900

    리스트 렌더링

--- 한입 리액트/simplediary/src/App.js
+++ 한입 리액트/simplediary/src/App.js
@@ -1,11 +1,37 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
 function App() {
+  const dummyList = [
+    {
+      id: 1,
+      author: "조성찬",
+      content: "하이 1",
+      emotion: 5,
+      creted_date: new Date().getTime(),
+    },
+    {
+      id: 2,
+      author: "아무개",
+      content: "하이 2",
+      emotion: 3,
+      creted_date: new Date().getTime(),
+    },
+    {
+      id: 3,
+      author: "홍길동",
+      content: "하이 3",
+      emotion: 1,
+      created_date: new Date().getTime(),
+    },
+  ];
+
   return (
     <div className="App">
       <h2>일기장</h2>
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ 한입 리액트/simplediary/src/DiaryItem.js
@@ -0,0 +1,15 @@
+const DiaryItem = ({ author, content, created_date, emotion, id }) => {
+  return (
+    <div className="DiaryItem">
+      <div className="info">
+        <span>
+          작성자 : {author} | 감정 : {emotion}{" "}
+        </span>
+        <br />
+        <span className="date">{new Date(created_date).toLocaleString()} </span>
+      </div>
+      <div className="content">{content}</div>
+    </div>
+  );
+};
+export default DiaryItem;
