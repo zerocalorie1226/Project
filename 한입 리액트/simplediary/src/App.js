0586ad9393fb5d0e1a7166dbfb687098384b39d5
Sungchan CHo <kjyy07@naver.com>
2023-11-27 16:30:57 +0900

    React에서 배열 사용하기 4 - 데이터 수정하기

--- 한입 리액트/simplediary/src/App.js
+++ 한입 리액트/simplediary/src/App.js
@@ -26,10 +26,18 @@ function App() {
     setData(newDiaryList);
   };
 
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} onRemove={onRemove} />
+      <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
 }
