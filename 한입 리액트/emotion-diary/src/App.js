67a0da2baa2aceb232eeb4d83e181b41cbeaef94
Sungchan CHo <kjyy07@naver.com>
2023-11-28 16:32:34 +0900

    프로젝트 기초 공사 1

--- 한입 리액트/emotion-diary/src/App.js
+++ 한입 리액트/emotion-diary/src/App.js
@@ -4,21 +4,46 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
-
-import RoutesTest from "./components/RouteTest";
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}
+            />
+          }
+        />
         <h2>App.js</h2>
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RoutesTest />
       </div>
     </BrowserRouter>
   );
