615dde557c8a7d781116714a833ef8d65ba968f8
Sungchan CHo <kjyy07@naver.com>
2023-11-28 15:24:02 +0900

    페이지 라우팅 2 - React Router 응용

--- 한입 리액트/emotion-diary/src/pages/Edit.js
+++ 한입 리액트/emotion-diary/src/pages/Edit.js
@@ -1,8 +1,23 @@
+import { useSearchParams, useNavigate } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+
+  const mode = searchParams.get("mode");
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: "조성찬" })}>
+        QS바꾸기
+      </button>
+      <button onClick={() => navigate("/home")}>Home으로 가기</button>
+      <button onClick={() => navigate(-1)}>뒤로 가기</button>
     </div>
   );
 };
